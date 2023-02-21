5e561311353b87fe1a3a2784c3d57d3fb8dfd936
lovavikasso <victoria.pavlova7@gmail.com>
2023-02-21 21:20:53 +0300

    Homework 2nd week Tuesday 1try

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -5,9 +5,9 @@ import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
 
-/*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
- * 2 - дописать логику функции restore
+/* 0 раскомментировать в HW5
+ * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack OK
+ * 2 - дописать логику функции restore OK
  * 3 - сделать стили в соответствии с дизайном
  */
 
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        restoreState<string>('hw6-editable-span-value', value)
+        setValue(restoreState<string>('hw6-editable-span-value', value))
     }
 
     return (
