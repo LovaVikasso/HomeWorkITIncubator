2dbe60a608af43e0cce935868cc19027c224e195
lovavikasso <victoria.pavlova7@gmail.com>
2023-03-01 11:25:13 +0300

    Homework 4th week Tuesday 1try

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,26 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
+        case 'sort': {  // by name
+            const copyState = [...state]
+            if (action.payload === 'up') {
+                copyState.sort((a, b) =>
+                    a.name === b.name ? 0 : a.name > b.name ? 1 : -1
+                )
+                return copyState // need to fix
+            } else if (action.payload === 'down') {
+                copyState.sort((a, b) =>
+                    a.name === b.name ? 0 : a.name < b.name ? 1 : -1
+                )
+                return copyState // need to fix
+            }
+            return copyState
+         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(man => man.age >= action.payload)
+             // need to fix
         }
         default:
             return state
