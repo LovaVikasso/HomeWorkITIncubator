5e2e50836dfa42a4a49963f7cdf7f770c43e0ed9
lovavikasso <victoria.pavlova7@gmail.com>
2023-06-12 19:08:38 +0300

    Refactor

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,13 +17,13 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
+
     const finalClassName = s.button
-        + (disabled  ? ' ' + s.disabled
-            : xType === 'red'
-                ? ' ' + s.red :
-                xType === 'secondary'
-                    ? ' ' + s.secondary :
-                    ' ' + s.default)// задачка на смешивание классов
+        + (disabled ? ' ' + s.disabled
+            : xType === 'default' ? ' ' + s.default
+                : xType === 'red' ? ' ' + s.red
+                    : xType === 'secondary' ? ' ' + s.secondary
+                        : '')
 
     return (
         <button
