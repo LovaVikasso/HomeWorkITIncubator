3104dc1eb1f82a4e0bf00eda1d46bb80e0ff7524
lovavikasso <victoria.pavlova7@gmail.com>
2023-06-14 11:39:25 +0300

    Homework 14 1 try

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -16,7 +16,7 @@ import {useSearchParams} from 'react-router-dom'
 const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
-            'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
+            'https://samurai.it-incubator.io/api/3.0/homework/test2',
             {params: {find}}
         )
         .catch((e) => {
@@ -35,20 +35,19 @@ const HW14 = () => {
         getTechs(value)
             .then((res) => {
                 // делает студент
-
+                setLoading(false)
+                if (res) setTechs(res.data.techs)
                 // сохранить пришедшие данные
-
-                //
             })
     }
 
     const onChangeText = (value: string) => {
         setFind(value)
         // делает студент
-
+        setLoading(false)
         // добавить/заменить значение в квери урла
         // setSearchParams(
-
+        setSearchParams({find:value})
         //
     }
 
@@ -67,7 +66,6 @@ const HW14 = () => {
     return (
         <div id={'hw14'}>
             <div className={s2.hwTitle}>Homework #14</div>
-
             <div className={s2.hw}>
                 <SuperDebouncedInput
                     id={'hw14-super-debounced-input'}
--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -32,8 +32,13 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
         onChangeText?.(value)
 
         if (onDebouncedChange) {
-            // делает студент
-
+                // делает студент
+                clearTimeout(timerId)
+                // остановить предыдущий таймер
+                // запустить новый на 1500ms, в котором вызовется функция
+                const ID = setTimeout(() => onDebouncedChange(value), 1500);
+                //
+                setTimerId(+ID)
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
 
